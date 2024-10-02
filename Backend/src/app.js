e889533f9bcf8b9ea65860dede12763c8d9eb883
LucasPandian <lucaspandian2017@gmail.com>
2024-10-02 12:18:19 -0300

    estruturação de pastas

---
+++ Backend/src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+const routersUsers = require("./Routers/routersUser");
+const routerLogin = require('./Routers/routerLogin');
+
+app.use(routersUsers);
+app.use(routerLogin);
+
+module.exports = app;
